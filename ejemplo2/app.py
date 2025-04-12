e0e1dd0e6cd9f8f5d5d5b47963792141b632b2d2
Kivan M. López Raíces <kivanmiguel2206@gmail.com>
2025-04-12 09:49:42 -0400

    update file

---
+++ ejemplo2/app.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route("/", methods=["GET"])
+def home():
+    return "bienvenido a mi api"
+
+@app.route("/saludo", methods=["POST"])
+def saludo():
+    data = request.json
+    nombre = data.get("nombre", "Usuario")
+    return f"Hola, {nombre}!"
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ ejemplo3/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+todos = {"Todos": ["No","Tienes", "Descuento", "Skill Issue"] }
+
+@app.route("/", methods=["GET"])
+def home():
+    return "simple TODO api"
+
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    return jsonify(todos)
+
+@app.route("/todos", methods=["POST"])
+def insert_todos():
+    data = request.json
+    
+    if not data or "todo" not in data:
+        return jsonify({"error" : "Datos incompletos"})
+    
+    todos["todos"].append(data['todo'])
+    return jsonify(todos)
+
+if __name__ == "__main__":
+    app.run(debug=True)

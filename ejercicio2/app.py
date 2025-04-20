edff9139a44c38bb09f79f3ab6af6280fef4bb05
Kivan M. López Raíces <kivanmiguel2206@gmail.com>
2025-04-20 17:20:54 -0400

    update file

---
+++ ejercicio2/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# ejercicio2 
+usuarios = []
+
+# Ruta GET para obtener información del servidor
+@app.route("/info", methods=["GET"])
+def info():
+    return jsonify({
+        'ejercicio': "2",
+        'usuario': 'Kivan M. Lopez'
+    })
+
+# Ruta POST /crear_usuario
+@app.route('/crear_usuario', methods=['POST'])
+def crear_usuario():
+    data = request.json
+
+    nombre = data.get('nombre')
+    correo = data.get('correo')
+
+    if not nombre or not correo:
+        return jsonify({
+            'ERROR': 'Es debido tener ambos datos: nombre y correo'
+        }), 400
+
+    nuevo_usuario = {
+        'id': len(usuarios) + 1,
+        'nombre': nombre,
+        'correo': correo
+    }
+
+    usuarios.append(nuevo_usuario)
+
+    return jsonify({
+        "mensaje": "Usuario creado exitosamente!",
+        "usuario": nuevo_usuario
+    }), 201
+
+# Ruta GET /usuarios
+@app.route('/usuarios', methods=['GET'])
+def listar_usuarios():
+    return jsonify(usuarios)
+
+# Corrección final para ejecutar Flask
+if __name__ == '__main__':  # ← aquí estaba el error: era __name__ y no name
+    app.run(debug=True)
